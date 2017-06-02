489bf1c98e0063c35d8bd972f95fcf327bb646f7
dhruba619 <dhruba619@gmail.com>
2017-06-02 17:20:54 +0530

    Logging basic infrastructure

--- backend/src/main/java/org/collectiveone/C1ApiApp.java
+++ backend/src/main/java/org/collectiveone/C1ApiApp.java
@@ -1,12 +1,19 @@
 package org.collectiveone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class C1ApiApp { 
-
+    public static Logger logger = LoggerFactory.getLogger(C1ApiApp.class);
 	public static void main(String[] args) {
+	    //Remove the logs if required, this just to demo
+	    logger.info("Starting Application");
+	    logger.debug("Starting Application");
+	    logger.error("Starting Application");
+	    logger.warn("Starting Application");
 		SpringApplication.run(C1ApiApp.class, args);
 	}
 
